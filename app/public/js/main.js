da99ed37dc32b03beb0abcd45c94230cb14267f3
Tak Tran <contact@tutaktran.com>
2013-11-23 16:56:57 +0000

    Show accelerometer graph.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,44 +2,48 @@
 (function (){
   'use strict';
 
-  var PRESSURE_SENSOR_THRESHOLD = 100;
+  var SENSOR_THRESHOLD = 100;
+  var GRAPH_WIDTH = 300;
+  var GRAPH_HEIGHT = 300;
 
   $(function() {
     var sensorData = {
       pressure: [],
       accelerometer: {
-        x: [],
-        y: [],
-        z: []
+        x: [{x: 0, y: 0}],
+        y: [{x: 0, y: 0}],
+        z: [{x: 0, y: 0}]
       }
     };
-    var timeCounter = 0;
-    var graph;
+    var pressureTimeCounter = 0;
+    var pressureGraph;
+    var accelerometerTimeCounter = 0;
+    var accelerometerGraph;
 
     // ----------------------------------------
     // Set up graphs
     // ----------------------------------------
 
-    var initSensorData = function() {
-      for (var i = 0; i <= PRESSURE_SENSOR_THRESHOLD; i++) {
+    var initPressureSensorData = function() {
+      for (var i = 0; i <= SENSOR_THRESHOLD; i++) {
         sensorData["pressure"].push({
-          x: timeCounter++,
+          x: pressureTimeCounter++,
           y: 1
         });
       }
     };
 
     var initPressureGraph = function() {
-      initSensorData();
+      initPressureSensorData();
 
       var palette = new Rickshaw.Color.Palette({
         scheme: 'colorwheel'
       });
 
-      graph = new Rickshaw.Graph({
-        element: document.getElementById("graph"),
-        width: 470,
-        height: 300,
+      pressureGraph = new Rickshaw.Graph({
+        element: document.getElementById("pressure-graph"),
+        width: GRAPH_WIDTH,
+        height: GRAPH_HEIGHT,
         renderer: 'line',
         stroke: true,
         preserve: true,
@@ -47,17 +51,17 @@
           {
             color: palette.color(),
             data: sensorData["pressure"],
-            name: 'x-axis'
+            name: 'pressure'
           }
         ]
       });
 
-      graph.render();
+      pressureGraph.render();
 
       var ticksTreatment = 'glow';
 
       var xAxis = new Rickshaw.Graph.Axis.Time({
-        graph: graph,
+        graph: pressureGraph,
         ticksTreatment: ticksTreatment,
         timeFixture: new Rickshaw.Fixtures.Time.Local()
       });
@@ -65,7 +69,60 @@
       xAxis.render();
 
       var yAxis = new Rickshaw.Graph.Axis.Y({
-        graph: graph,
+        graph: pressureGraph,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+        ticksTreatment: ticksTreatment
+      });
+
+      yAxis.render();
+    };
+
+    var initAccelerometerGraph = function() {
+
+      var palette = new Rickshaw.Color.Palette({
+        scheme: 'colorwheel'
+      });
+
+      accelerometerGraph = new Rickshaw.Graph({
+        element: document.getElementById("accelerometer-graph"),
+        width: GRAPH_WIDTH,
+        height: GRAPH_HEIGHT,
+        renderer: 'line',
+        stroke: true,
+        preserve: true,
+        series: [
+          {
+            color: palette.color(),
+            data: sensorData["accelerometer"]["x"],
+            name: 'x-axis'
+          },
+          {
+            color: palette.color(),
+            data: sensorData["accelerometer"]["y"],
+            name: 'y-axis'
+          },
+          {
+            color: palette.color(),
+            data: sensorData["accelerometer"]["z"],
+            name: 'z-axis'
+          }
+        ]
+      });
+
+      accelerometerGraph.render();
+
+      var ticksTreatment = 'glow';
+
+      var xAxis = new Rickshaw.Graph.Axis.Time({
+        graph: accelerometerGraph,
+        ticksTreatment: ticksTreatment,
+        timeFixture: new Rickshaw.Fixtures.Time.Local()
+      });
+
+      xAxis.render();
+
+      var yAxis = new Rickshaw.Graph.Axis.Y({
+        graph: accelerometerGraph,
         tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
         ticksTreatment: ticksTreatment
       });
@@ -74,6 +131,7 @@
     };
 
     initPressureGraph();
+    initAccelerometerGraph();
 
     // ----------------------------------------
     // Web sockets server
@@ -117,7 +175,33 @@
       if (data) {
         var accel = data["accelerometer"];
         if (accel) {
-          // console.table(accel);
+          var accelData = sensorData["accelerometer"];
+          var newX = accelerometerTimeCounter++;
+
+          // TODO: Figure out how to do it with native values (remove * -100)
+          // TODO: Show both values of accel [0] and [1]
+          accelData["x"].push({
+            x: newX,
+            y: accel["x"][0] * -100
+          });
+
+          accelData["y"].push({
+            x: newX,
+            y: accel["y"][0] * -100
+          });
+
+          accelData["z"].push({
+            x: newX,
+            y: accel["z"][0] * -100
+          });
+
+          if (accelData.length > SENSOR_THRESHOLD) {
+            accelData["x"].shift();
+            accelData["y"].shift();
+            accelData["z"].shift();
+          }
+
+          accelerometerGraph.update();
         }
 
         var pressure = data["pressure"];
@@ -125,15 +209,15 @@
           var pressureData = sensorData["pressure"];
 
           pressureData.push({
-            x: timeCounter++,
+            x: pressureTimeCounter++,
             y: pressure.value
           });
 
-          if (pressureData.length > PRESSURE_SENSOR_THRESHOLD) {
+          if (pressureData.length > SENSOR_THRESHOLD) {
             pressureData.shift();
           }
 
-          graph.update();
+          pressureGraph.update();
         }
       }
     });
